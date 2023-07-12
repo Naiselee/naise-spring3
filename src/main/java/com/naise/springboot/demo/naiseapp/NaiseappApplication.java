1ed973e0ad698956590e1f36dce5b27df8f98db2
Adrian Naise <adriannaise@gmail.com>
2023-07-12 08:48:30 +0200

    Initial commit

---
+++ src/main/java/com/naise/springboot/demo/naiseapp/NaiseappApplication.java
@@ -0,0 +1,13 @@
+package com.naise.springboot.demo.naiseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaiseappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaiseappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naise/springboot/demo/naiseapp/NaiseappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naise.springboot.demo.naiseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaiseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
